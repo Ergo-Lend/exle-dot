c2a001d3b31c916876bca971d6904cc34148b81a
Keith Lim <keithlimweiluen@gmail.com>
2022-05-22 01:28:44 -0700

    reenabled test

--- modules/single-lender/src/test/scala/SLErgs/ContractsSpec.scala
+++ modules/single-lender/src/test/scala/SLErgs/ContractsSpec.scala
@@ -10,7 +10,7 @@ import org.scalatest.wordspec.AnyWordSpec
 class ContractsSpec extends AnyWordSpec with Matchers {
   "getContracts" when {
     "getting Exle Contracts" should {
-      "get the right contracts" ignore {
+      "get the right contracts" in {
         val dummyErgoScript = "{\n" +
           "  // this is a test\n" +
           "}"
